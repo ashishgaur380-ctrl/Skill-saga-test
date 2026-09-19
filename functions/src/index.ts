68f8b055bfac9aa602bcef637f7233dc1b3262d0
ashishgaur380-ctrl <ashishgaur380@gmail.com>
2026-09-20 01:56:14 +0530

    Export reward functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -44,3 +44,10 @@ export {
   moderateCommunityPost,
   archiveCommunityPost,
 } from "./community";
+
+export {
+  listRewards,
+  createReward,
+  updateReward,
+  archiveReward,
+} from "./rewards";
