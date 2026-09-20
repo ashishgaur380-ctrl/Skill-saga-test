7e450294fddb69c778d6ad0db06aef8b3b1e300e
ashishgaur380-ctrl <ashishgaur380@gmail.com>
2026-09-20 15:25:55 +0530

    fix: expose role aware notification functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,110 +1,20 @@
 import { initializeApp } from "firebase-admin/app";
-
 initializeApp();
-
-export {
-  listAcademic,
-  createAcademic,
-  updateAcademic,
-  archiveAcademic,
-  bulkImportAcademic,
-} from "./academic";
-
-export {
-  listQuestions,
-  createQuestion,
-  updateQuestion,
-  archiveQuestion,
-  bulkImportQuestions,
-} from "./question";
-
-export {
-  listQuizzes,
-  createQuiz,
-  updateQuiz,
-  archiveQuiz,
-} from "./quiz";
-
-export {
-  listCompetitions,
-  createCompetition,
-  updateCompetition,
-  archiveCompetition,
-} from "./competition";
-
-export {
-  listAutomationRules,
-  createAutomationRule,
-  updateAutomationRule,
-  setAutomationEnabled,
-  runAutomationEngine,
-  runAutomationEngineNow,
-} from "./automation";
-
-export {
-  listCommunityPosts,
-  createCommunityPost,
-  moderateCommunityPost,
-  archiveCommunityPost,
-} from "./community";
-
-export {
-  listRewards,
-  createReward,
-  updateReward,
-  archiveReward,
-  listRewardRedemptions,
-  updateRewardRedemptionStatus,
-} from "./rewards";
-
-export {
-  listNotificationTemplates,
-  createNotificationTemplate,
-  updateNotificationTemplate,
-  archiveNotificationTemplate,
-} from "./notifications";
-
+export { listAcademic, createAcademic, updateAcademic, archiveAcademic, bulkImportAcademic } from "./academic";
+export { listQuestions, createQuestion, updateQuestion, archiveQuestion, bulkImportQuestions } from "./question";
+export { listQuizzes, createQuiz, updateQuiz, archiveQuiz } from "./quiz";
+export { listCompetitions, createCompetition, updateCompetition, archiveCompetition } from "./competition";
+export { listAutomationRules, createAutomationRule, updateAutomationRule, setAutomationEnabled, runAutomationEngine, runAutomationEngineNow } from "./automation";
+export { listCommunityPosts, createCommunityPost, moderateCommunityPost, archiveCommunityPost } from "./community";
+export { listRewards, createReward, updateReward, archiveReward, listRewardRedemptions, updateRewardRedemptionStatus } from "./rewards";
+export { listNotificationTemplates, createNotificationTemplate, updateNotificationTemplate, archiveNotificationTemplate } from "./notifications";
 export { getAnalyticsSummary } from "./analytics";
-
 export { getSystemSettings, updateSystemSettings } from "./system";
-
 export { listUsers, updateUser } from "./users";
-
-export {
-  listPublishedQuizzes,
-  getQuizForAttempt,
-  submitQuizAttempt,
-  getLearnerStats,
-  listLearnerAttempts,
-  getLearnerHome,
-  listLearnerNotifications,
-  markNotificationRead,
-  getLearnerAcademic,
-  getTopicPractice,
-  submitTopicPractice,
-  getLearnerProgress,\n  listLearnerAssignments,\n  listCommunityFeed,
-  listPublishedCompetitions,
-  joinCompetition,
-  getCompetitionQuiz,
-  submitCompetitionAttempt,
-  getCompetitionLeaderboard,
-} from "./learner";
-export { getLearnerRewards, redeemReward } from "./learner";
-
+export { listPublishedQuizzes, getQuizForAttempt, submitQuizAttempt, getLearnerStats, listLearnerAttempts, getLearnerHome, listLearnerNotifications, markNotificationRead, getLearnerAcademic, getTopicPractice, submitTopicPractice, getLearnerProgress, listLearnerAssignments, listCommunityFeed, listPublishedCompetitions, joinCompetition, getCompetitionQuiz, submitCompetitionAttempt, getCompetitionLeaderboard, getLearnerRewards, redeemReward } from "./learner";
 export { createLearnerLinkCode, linkLearner, listLinkedLearners, getLinkedLearnerProgress } from "./guardian";
-
-export {
-  listLearningMaterials,
-  createLearningMaterial,
-  updateLearningMaterial,
-  archiveLearningMaterial,
-  bulkCreateLearningMaterials,
-} from "./content";
-
+export { listLearningMaterials, createLearningMaterial, updateLearningMaterial, archiveLearningMaterial, bulkCreateLearningMaterials } from "./content";
 export { createAssignment, listAssignments, updateAssignment, archiveAssignment } from "./assignment";
-
 export { getTeacherDashboard, listTeacherAssignments, listTeacherMaterials, createTeacherMaterial, updateTeacherMaterial } from "./teacher";
-
 export { getSchoolDashboard, listSchoolMembers, updateSchoolMember, listSchoolAssignments } from "./school";
-
-export { sendNotification } from "./notification-delivery";
+export { sendNotification, listRecipientNotifications, markRecipientNotificationRead } from "./notification-delivery";
